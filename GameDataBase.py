5def2c071c8fc657d1f85958a26f302392789f79
a-float <mati852456@gmail.com>
2020-11-15 18:54:47 +0100

    added sorting and console interface

--- GameDataBase.py
+++ GameDataBase.py
@@ -1,39 +1,45 @@
-
+import json
 import tabulate as tb
 
 #TODO add max data size
 
 class GameDataBase():
-	headers = ["name","size","company","rating","price"]
-	types = [str, float, str, float, float]
 	def __init__(self):
 		self.data = {}
+		self.headers = ["name","size","company","rating","price"]
+		self.types = {"name":str, "size":float, "company":str, "rating":float, "price":float}
 
 	def parse_input(self, raw_data):
-		if len(raw_data) != len(GameDataBase.headers):
+		if len(raw_data) != len(self.headers):
 			return []
-		types = GameDataBase.types;
-		clean_data = [None]*len(raw_data)
-		for i in range(len(raw_data)):
-			try:
-				if types[i] == float:
-					clean_data[i] = float(raw_data[i])
-				elif types[i] == str and raw_data[i] != "":
-					clean_data[i] = str(raw_data[i])
-			except:
-				print("ivalid input, defaults to None")
+		clean_data = {h : None for h in self.headers}
+
+		for i in range(len(self.headers)):
+			h = self.headers[i]
+			if self.types[h] == float:
+				try:
+					clean_data[h] = float(raw_data[i])
+				except:
+					print(f"{h} is not a string, defaults to 'null'")
+					clean_data[h] = 'null'
+			elif self.types[h] == str and raw_data[i] != "":
+				try:
+					clean_data[h] = str(raw_data[i])
+				except:
+					print(f"{h} is not a float, defaults to 0")
+					clean_data[h] = 0
+		print(clean_data)
 		return clean_data
 
 	def add_record(self, raw_data):
-		headers = GameDataBase.headers
-		clean_data = GameDataBase.parse_input(self, raw_data)
+		clean_data = self.parse_input(raw_data)
 		if clean_data == []:
 			return False
 		i = 0	#finding the smalles available id
 		while i in self.data.keys():	#may be changed later
 			i+=1
-		print(clean_data)
-		self.data.update({i : clean_data}) #adding the new record
+		# print(clean_data)
+		self.data.update({i : {k:v for k,v in zip(self.headers,clean_data)}}) #adding the new record
 
 		print("-added new record at index {}".format(i))
 
@@ -49,17 +55,57 @@ class GameDataBase():
 		if len(self.data) == 0:
 			print("-the database is empty.")
 		else: 
-			table = self.data.values()
-			print(tb.tabulate(table, headers="keys", numalign="right", showindex=True,tablefmt="psql", floatfmt=("id","name",".1f","comp",".1f",".2f")))
+			print(self.data)
+			table = [[k]+list(v.values()) for k,v in self.data.items()]		#used to be psql
+			print(tb.tabulate(table, headers="keys", numalign="right", showindex=True, floatfmt=("id","name",".1f","comp",".1f",".2f")))
 
-# if __name__ != '__main__':
+	def input_record(self):
+		res = []
+		for h in self.headers:
+			res.append(input(f"Input {h} ({self.types[h]}): "))
+		self.add_record(res)
+
+	def input_remove(self):
+		i = input("Type the id of the record to be deleted: ")
+		try:
+			i = int(i)
+			gdb.remove_record(i)
+		except:
+			print("Invalid record id")
+	
+	# def save_data(self):
+	# def load_data(self):	
+
+
 gdb = GameDataBase()
 gdb.show()
 gdb.add_record(["Battlefield", 30, "EA", 6.5, 69.99])
 gdb.add_record(["Skyrim", 50, "Bethesda", 9, 45.99])
-gdb.add_record(["IceTower", 0.5, "Bethesda", 3.7, 3.89])
+gdb.add_record(["IceTower", 0.5, "Bethesda", 3.7, 0])
 gdb.add_record(["ImmortalPlanet", 2, "MonsterCouch", 9, 19.99])
 gdb.add_record(["Fallout 97", 34.8, "Bethesda", 9, 37.13])
 gdb.add_record(["Nostale", 3.2, "Gameforge", 8.3, 0])
 	# gdb.remove_record(1)
-	# gdb.show()
+gdb.show()
+
+def loop():
+	print("---The Game Data Base---")
+	print("Type ADD to add a new record.")
+	print("Type VIEW to display the table.")
+	print("Type REM to remove a record.")
+	print("Type EXIT to close the program.")
+	while True:
+		choice = input("Choose action: ")
+		if choice == "ADD":
+			gdb.input_record()
+		elif choice == "VIEW":
+			gdb.show()
+		elif choice == "REM":
+			gdb.input_remove();
+		elif choice == "EXIT":
+			break;
+		else:
+			print("Command unknown")
+
+if __name__ == '__main__':
+	loop()
--- mainLoop.py
+++ mainLoop.py
@@ -1,94 +1,121 @@
 import PySimpleGUI as sg
-from piotroprojekt import *
+from GameDataBase import *
 
 sg.theme('Default1')
 
 # ------ Make the Table Data ------
-def createTableData():
-	data = list(gdb.data.values())
-	print("The data is:")
+def createTableData(gdb, sortby = "id", sortdir = "ascending"):
+	print(f"-sorting by {sortby} {sortdir}")
+	rev = False if sortdir=="ascending" else True
+
+	if sortby != "id":
+		sort_key = lambda x: x[1][sortby]
+	else:
+		sort_key = lambda x: x[0]
+	sorted_dict = {k: v for k, v in sorted(gdb.data.items(), key=sort_key, reverse=rev)}
+	data = [[k]+list(v.values()) for k,v in sorted_dict.items()]
 	print(data)
-	print("original data:")
-	print(gdb.data)
 	for row in data:		#TODO possibly change it so its not that hardcoded like this
-		print(row)
-		if row[1]:
-			row[1] = "{:.1f}".format(float(row[1]))
-		if row[3]:
-			row[3] = "{:.1f}".format(float(row[3]))
-		if row[4]:
-			row[4] = "{:.2f}".format(float(row[4]))
+		if row[1+1] is not None:
+			row[1+1] = "{:.1f}".format(float(row[1+1]))
+		if row[3+1] is not None:
+			row[3+1] = "{:.1f}".format(float(row[3+1]))
+		if row[4+1] is not None:
+			row[4+1] = "{:.2f}".format(float(row[4+1]))
 	return data
 
-def createHeaders():
-	heds = GameDataBase.headers[:] #using [:] to make a copy of the list
+def createHeaders(gdb):
+	heds = gdb.headers[:] #using [:] to make a copy of the list
 	heds[1] += ' [Gb]'
 	heds[4] += ' [zł]'
+	heds = ["id"] + heds
 	return heds
 
+def createLayout(gdb):
+	leftPanel = sg.Table(values=createTableData(gdb), headings=createHeaders(gdb), max_col_width=25,
+						# background_color='light blue',
+						# auto_size_columns=True,
+						display_row_numbers=True,
+						justification='center',
+						num_rows=18,
+						alternating_row_color='grey90',
+						key='-TABLE-',
+						row_height=24,
+						selected_row_colors=('black','SteelBlue2'),
+						header_background_color='SteelBlue2',
+						vertical_scroll_only=False,
+						enable_events=True,
+						tooltip='Ma tabela')
+	
+	addRemoveFrame = sg.Frame(title="Record management", layout=[[sg.Button('Add record'), sg.Button('Remove records')]])
+	sortingFrame = sg.Frame(title="Sort by", layout= [[ 
+							 sg.Listbox(["id"] + gdb.headers,
+								   size=(11,len(gdb.headers)+1),
+								   default_values="id",
+								   select_mode='LISTBOX_SELECT_MODE_SINGLE',
+								   enable_events=True, 
+								   k='-SORTBY-'),
+						 	 sg.Listbox(["ascending", "descending"], 
+								   size=(12,2),
+								   default_values="ascending",
+								   select_mode='LISTBOX_SELECT_MODE_SINGLE',
+								   enable_events=True,
+								   k='-SORTDIR-')]])
+	rightContent = [
+					  [addRemoveFrame],
+					  [sortingFrame]
+				   ]
 
-# ------ Window Layout ------
-leftPanel = sg.Table(values=createTableData(), headings=createHeaders(), max_col_width=25,
-					# background_color='light blue',
-					# auto_size_columns=True,
-					display_row_numbers=True,
-					justification='center',
-					num_rows=18,
-					alternating_row_color='grey90',
-					key='-TABLE-',
-					row_height=24,
-					selected_row_colors=('black','SteelBlue2'),
-					header_background_color='SteelBlue2',
-					vertical_scroll_only=False,
-					enable_events=True,
-					tooltip='Ma tabela')
-rightContent = [
-				  [sg.Button('Read'), sg.Button('Double'), sg.Button('New record'), sg.Button('New record')],
-				  [sg.Text('Read = read which rows are selected')],
-				  [sg.Text('Double = double the amount of data in the table')],
-				  [sg.Text('Change Colors = Changes the colors of rows 8 and 9')]
-			   ]
+	rightPanel = sg.Column(rightContent, 
+							vertical_alignment="top",
+							pad=(5,10),
+							# expand_y=True,
+							# background_color='lightyellow'
+							)
 
-rightPanel = sg.Column(rightContent, 
-						vertical_alignment="top",
-						pad=(5,10),
-						# expand_y=True,
-						background_color='lightyellow')
+	layout = [[leftPanel, sg.VSeperator(color='grey90'), rightPanel]]
+	return layout
 
-layout = [[leftPanel, sg.VSeperator(color='grey90'), rightPanel]]
-
-# ------ Create Window ------
-window1 = sg.Window('The Table Element', layout=layout, font=('Helvetica', 11), finalize=True)
-window2 = None
-
-def createInputWindow():
-	layout = [[sg.Text("{:10}:".format(h)), sg.Input(h[1:4])] for h in createHeaders()]
+def createInputWindow(gdb):
+	layout = [[sg.Text("{:10}:".format(h)), sg.Input(h[1:4])] for h in gdb.headers]
 	layout.append([sg.Button("Add"), sg.Button("Cancel")])
 	return sg.Window("Add a new record", keep_on_top="True", layout=layout, finalize=True)
 
-# ------ Event Loop ------
-while True:
-	window, event, values = sg.read_all_windows()
-	print(window, event, values)
-	if event == sg.WIN_CLOSED:
-		window.close()
-		if window == window2:       # if closing win 2, mark as closed
+def GUI(gdb):
+	window1 = sg.Window('The Table Element', layout=createLayout(gdb), font=('Helvetica', 14), finalize=True)
+	window2 = None
+
+	while True:
+		window, event, values = sg.read_all_windows()
+		# print(event, values)
+		if event == sg.WIN_CLOSED:
+			window.close()
+			if window == window2:       # if closing win 2, mark as closed
+				window2 = None
+			elif window == window1:     # if closing win 1, exit program
+				break
+		elif window == window2:
+			if event == "Add":  #TODO add type parsing
+				gdb.add_record(list(values.values())) #values is a dict with keys 0,1,2,3,..,len(inputs)
+				print("Adding")
+				window1['-TABLE-'].update(values = createTableData(gdb))
+			window.close()  #close window2 anyway
 			window2 = None
-		elif window == window1:     # if closing win 1, exit program
-			break
-	elif window == window2:
-		if event == "Add":  #TODO add type parsing
-			print(list(values.values()))
-			gdb.add_record(list(values.values())) #values is a dict with keys 0,1,2,3,..,len(inputs)
-			print("Adding")
-			window1['-TABLE-'].update(values = createTableData())
-		window.close()  #close window2 anyway
-		window2 = None
-	elif window == window1:
-		if event == 'Double':
-			for i in range(len(data)):
-				data.append(data[i])
-			window['-TABLE-'].update(values=data)
-		elif event == 'New record' and not window2:
-			window2 = createInputWindow()
-window.close()
+		elif window == window1:
+			if event == 'Add record' and not window2:
+				window2 = createInputWindow(gdb)
+			elif event in ['-SORTBY-','-SORTDIR-']:
+				window1['-TABLE-'].update(values = createTableData(gdb, values['-SORTBY-'][0], values['-SORTDIR-'][0]))
+			elif event == "Remove records":
+				ids_to_remove = sg.popup_get_text("Input multiple ids separated by space:", title="Remove record", size=(30,45))
+				print(f"-removing {ids_to_remove}")
+				for i in ids_to_remove.split(' '):
+					try:
+						i = int(i)
+					except:
+						print(f"invalid id {i}")
+					gdb.remove_record(i)
+				window1['-TABLE-'].update(values = createTableData(gdb))
+	window.close()
+
+GUI(gdb)
